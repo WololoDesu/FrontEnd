d7ad66354f758022ca640b8be8e7211943c022bd
vasseuvi <valentin.vasseur41@gmail.com>
2017-03-26 13:34:27 +0200

    Update chiisai.js

--- espaceMembre/assets/js/chiisai.js
+++ espaceMembre/assets/js/chiisai.js
@@ -1,3 +1,3 @@
-cheet('a m e y u k i', function() {
+cheet('l i t t l e p a n d a', function() {
     document['location']['href'] = 'https://www.youtube.com/tv#/watch/video/control?v=PJks_cSjVx8&resume'
 })
